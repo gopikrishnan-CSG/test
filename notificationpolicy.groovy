8eb598bcf130ee594ebef5d50ff9e0fcc7d46033
Gopi Krishnan <110552215+gopikrishnan-CSG@users.noreply.github.com>
2023-01-11 16:31:49 +0530

    Create notificationpolicy.groovy

---
+++ notificationpolicy.groovy
@@ -0,0 +1,11 @@
+def notificationpolicy()
+
+node {
+    stage('Display Notifications Summary') {
+        sh """curl --location --request GET 'https://api.opsgenie.com/v2/policies/notification?teamId=e27f4934-aa2f-41b4-ba2a-d149368285ac' --header 'Authorization: GenieKey 5b2c51fb-eaf2-4f3d-82a1-9138f356736d' > index.txt
+            cat index.txt | jq -r '.data'
+            """
+    }
+}
+
+return this
